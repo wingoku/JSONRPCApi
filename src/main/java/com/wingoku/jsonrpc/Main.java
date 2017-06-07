cf374ec293637ba5fe7a84b3abf558a7cc89ad10
Umer Farooq <ushersyoutube@yahoo.com>
2017-06-07 22:05:00 +0530

    Added: Calling server from main

--- src/main/java/com/wingoku/jsonrpc/Main.java
+++ src/main/java/com/wingoku/jsonrpc/Main.java
@@ -1,5 +1,6 @@
 package com.wingoku.jsonrpc;
 
+import com.wingoku.jsonrpc.server.WingokuServer;
 
 /**
  * Created by Umer on 6/6/2017.
@@ -7,6 +8,9 @@ package com.wingoku.jsonrpc;
 public class Main {
 
     public static void main(String[] args) {
+        // create and start server
+        WingokuServer server = new WingokuServer(5005);
+        server.setServerStatusListener(System.out::println);
+        server.startServer();
     }
-
 }
